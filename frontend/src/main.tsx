8d1b2c01052913001de8bf257b119c78b0225cb2
BuildTrack Ops <info@seifertcapital.com>
2026-07-22 19:53:43 +0000

    Apply-rich-navy-dashboard-and-Inter-font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/inter'
 import './index.css'
 import './operations-calendar-pro.css'
 import App from './App.tsx'
